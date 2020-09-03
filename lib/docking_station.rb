3de1c2d7c7edb844b4516d4718e0df720d648b01
Scott <Scotthall1983@hotmail.com>
2020-09-03 17:50:22 +0100

    Passed f-test by writing a failing unit test which ensures that bikes are allowed to be docked. Added dock_bike method which with Bike.new which passes the relevant feature and unit tests. Need to complete the 2nd user story which checks that a bike has been docked.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,12 @@
 require_relative "bike"
 
 class DockingStation
-
-  def initialize(bike)
-    @bike = bike
-  end
-
   def release_bike
     Bike.new
   end
 
   def dock_bike
-    @bike
+    Bike.new
   end
+
 end
