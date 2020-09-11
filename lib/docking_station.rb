70f5b6855a56e6554d935fd3815a29e5963b9dab
Scott <Scotthall1983@hotmail.com>
2020-09-11 09:24:49 +0100

    Minor refactor to remove the bike method and it's contents and replace it with an arr_reader which calls a bike instance.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,15 +2,15 @@ require_relative "bike"
 
 class DockingStation
 
+attr_reader :bike
+
   def release_bike
     Bike.new
   end
 
+# Use an instance variable to store the state of this instance.
   def dock(bike)
     @bike = bike
   end
 
-  def bike
-    @bike
-  end
 end
