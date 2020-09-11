a019f3b00c83c2a1723dc76eeb0f90af6b656726
Scott <Scotthall1983@hotmail.com>
2020-09-11 09:16:25 +0100

    Updated Learning.MD and wrote a new failing test which expects the docking station to return a docked bike.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,6 +10,8 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
+# updated this test, changed it from expect(subject.dock).to respond_to :dock.with(1).argument
+# Updated that test to the below.
   it 'expects to respond to dock with 1 argument' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
@@ -17,5 +19,11 @@ describe DockingStation do
 
   it 'is expected to respond to a bike instance' do
     expect(subject).to respond_to :bike
+  end
+
+  it 'is expected to return a docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike 
   end
 end
