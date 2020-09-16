874c3c1a3582798d3e38aa00e84e63cf832ae93a
Scott <Scotthall1983@hotmail.com>
2020-09-16 22:46:47 +0100

    Updated tests so that dock now has it's own describe block ready for the next set of tests & user stories.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,6 @@
 require "docking_station"
 
 describe DockingStation do
-  it 'returns true when asked if bike is working' do
-    bike = Bike.new
-    expect(bike).to be_working
-  end
-
-  it 'is expected to respond to a bike instance' do
-    expect(subject).to respond_to :bike
-  end
-
   describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
@@ -23,18 +14,26 @@ describe DockingStation do
   end
 
   describe '#dock' do
-    # updated this test, changed it from expect(subject.dock).to respond_to :dock.with(1).argument
-    # Updated that test to the below.
-    it 'expects to respond to dock with 1 argument' do
-      bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
-    end
-
     it 'is expected to return a docked bike' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.bike).to eq bike
     end
+
+    it 'expects to respond to dock with 1 argument' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+  end
+
+  it 'returns true when asked if bike is working' do
+    bike = Bike.new
+    expect(bike).to be_working
+  end
+
+
+  it 'is expected to respond to a bike instance' do
+    expect(subject).to respond_to :bike
   end
 
 end
